4277b3988ed313598a75b247504d3778cc77dd46
Eduardo Ceolin <Eduardo Ceolin>
2016-05-03 23:14:26 -0300

    Estrutura inicial do projeto. Please enter the commit message for your changes. Lines starting

---
+++ ilegraapp/ilegraapp-application/src/main/java/br/com/ilegra/ilegraapp/application/controllers/GerenciadorController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ilegra.ilegraapp.application.controllers;
+
+/**
+ *
+ * @author dudu
+ */
+public class GerenciadorController {
+ 
+    public static void main(String[] args){
+        
+    }
+    
+}
